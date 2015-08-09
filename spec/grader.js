80f8f80602a72e1e936ff09559175b81ef52ffcb
Ian Behrmann <ianjbehrmann@gmail.com>
2015-08-08 19:40:45 -0600

    Test finalized with answers

--- spec/grader.js
+++ spec/grader.js
@@ -9,3 +9,22 @@ describe('letter grader', function(){
       expect(code.letterGrader(58)).toEqual("F");
   });
 });
+
+describe('average score', function(){
+  it('takes a set of scores and returns the average grade', function(){
+      expect(code.averageScore([90, 95, 87, 60])).toEqual(83);
+  });
+});
+
+describe('median score', function(){
+  it('takes a set of scores and returns the median value', function(){
+      expect(code.medianScore([52,80,80,86,94])).toEqual(80);
+      expect(code.medianScore([1, 1, 2, 6, 6, 9])).toEqual(4);
+  });
+});
+
+describe('mode score', function(){
+  it('takes a set of scores and returns the mode value', function(){
+      expect(code.modeScore([82, 92, 75, 91, 92, 89, 95, 100, 86])).toEqual(92);
+  });
+});
--- src.js
+++ src.js
@@ -1,17 +1,44 @@
 module.exports = {
 
   letterGrader: function(grade){
-    if (grade >= 0 && grade < 60){
-        return "F";
-    }else if(grade >= 60 && grade < 70){
-        return "D";
-    }else if (grade >= 70 && grade < 80){
-        return "C";
-    }else if (grade >= 80 && grade < 90){
-        return "B";
-    }else if (grade >= 90){
-        return "A";
-    }
+      if (grade >= 0 && grade < 60){
+          return "F";
+      }else if(grade >= 60 && grade < 70){
+          return "D";
+      }else if (grade >= 70 && grade < 80){
+          return "C";
+      }else if (grade >= 80 && grade < 90){
+          return "B";
+      }else if (grade >= 90){
+          return "A";
+      }
   },
-  
+  averageScore: function(scores){
+      var avg = 0;
+      scores.forEach(function(grade){
+          avg += grade;
+      })
+      return avg / scores.length;
+  },
+  medianScore: function(scores){
+      var median = Math.floor((scores.length - 1) / 2);
+      if (scores.length % 2) {
+        return scores[median];
+      } else {
+        return (scores[median] + scores[median + 1]) / 2;
+      }
+  },
+  modeScore: function(scores){
+      var occur = {};
+      var max = 0; 
+      var result; 
+      for(var num in scores) {
+        occur[scores[num]]=(occur[scores[num]] || 0) + 1; 
+        if(occur[scores[num]] > max) {
+                max = occur[scores[num]];  
+                result = scores[num];     
+        }
+      }
+      return result;
+  }
 };
